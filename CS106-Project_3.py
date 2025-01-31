5a827eb9e0baa02e27a6688f550da8713e067bf0
NV <nvozmus@gmail.com>
2025-01-31 18:17:19 -0500

    optimized code

--- CS106-Project_3.py
+++ CS106-Project_3.py
@@ -1,9 +1,12 @@
 #NV Ozmus
 #Input
 ##request to enter number between 1-7
-def day(print("Enter a number 1 through 7 to calculate the corresponding day of the week"))
+## if number is 1, enter monday, 2 enter tuesday, 3 enter wednesday, 4 enter thursday, 5 enter friday, 6 enter saturday, 7 enter sunday
+#Output print the day and number
+
+day = int(print("Enter a number 1 through 7 to calculate the corresponding day of the week"))
 def dayofweek()
-#Process
+
 if day is 1, dayofweek = "Monday"
     elif day is 2, dayofweek = "Tuesday"
     elif day is 3, dayofweek = "Wednesday"
@@ -12,6 +15,5 @@ if day is 1, dayofweek = "Monday"
     elif day is 6, dayofweek = "Saturday"
     elif day is 7 dayofweek = "Sunday"
 else dayofweek = "not a valid entry"
-## if number is 1, enter monday, 2 enter tuesday, 3 enter wednesday, 4 enter thursday, 5 enter friday, 6 enter saturday, 7 enter sunday
-#Output print the day and number
+
 print("The day of the week you have entered is", dayofweek)

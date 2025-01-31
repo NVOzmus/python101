6906bd539081d651af5257460023b10ed7d96871
NV <nvozmus@gmail.com>
2025-01-31 16:59:31 -0500

    updated pseudocode

--- CS106-Project_3.py
+++ CS106-Project_3.py
@@ -1,4 +1,7 @@
 #NV Ozmus
 #Input
+##request to enter number between 1-7
+
 #Process
-#Output
+## if number is 1, enter monday, 2 enter tuesday, 3 enter wednesday, 4 enter thursday, 5 enter friday, 6 enter saturday, 7 enter sunday
+#Output print the day and number

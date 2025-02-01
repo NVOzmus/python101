0588fd2faa21287f73bfa6905defde9d4ac51ca8
NV <nvozmus@gmail.com>
2025-01-31 22:41:53 -0500

    finalized all pesudocode, cleaned up code.

--- CS106-Project_3.py
+++ CS106-Project_3.py
@@ -4,14 +4,14 @@
 ## if number is 1, enter monday, 2 enter tuesday, 3 enter wednesday, 4 enter thursday, 5 enter friday, 6 enter saturday, 7 enter sunday
 #Output print the day and number
 
-day = int(print("Enter a number 1 through 7 to calculate the corresponding day of the week"))
-if day is 1: dayofweek = "Monday"
-elif day is 2: dayofweek = "Tuesday"
-elif day is 3: dayofweek = "Wednesday"
-elif day is 4: dayofweek = "Thursday"
-elif day is 5: dayofweek = "Friday"
-elif day is 6: dayofweek = "Saturday"
-elif day is 7: dayofweek = "Sunday"
+day = int(input("Enter a number 1 through 7 to calculate the corresponding day of the week: "))
+if day == 1: dayofweek = "Monday"
+elif day == 2: dayofweek = "Tuesday"
+elif day == 3: dayofweek = "Wednesday"
+elif day == 4: dayofweek = "Thursday"
+elif day == 5: dayofweek = "Friday"
+elif day == 6: dayofweek = "Saturday"
+elif day == 7: dayofweek = "Sunday"
 else: dayofweek = "not a valid entry"
 
-print("The day of the week you have entered is", dayofweek)
+print("The day of the week you have entered:",day,"is", dayofweek)

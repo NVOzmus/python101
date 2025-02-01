141d73d97ed40d717a7add96c64b3ef6a14c4b40
NV <nvozmus@gmail.com>
2025-01-31 21:51:11 -0500

    further correct pseudo code syntax errors in process

--- CS106-Project_3.py
+++ CS106-Project_3.py
@@ -5,15 +5,13 @@
 #Output print the day and number
 
 day = int(print("Enter a number 1 through 7 to calculate the corresponding day of the week"))
-def dayofweek()
-
-if day is 1, dayofweek = "Monday"
-    elif day is 2, dayofweek = "Tuesday"
-    elif day is 3, dayofweek = "Wednesday"
-    elif day is 4, dayofweek = "Thursday"
-    elif day is 5, dayofweek = "Friday"
-    elif day is 6, dayofweek = "Saturday"
-    elif day is 7 dayofweek = "Sunday"
-else dayofweek = "not a valid entry"
+if day is 1: dayofweek = "Monday"
+elif day is 2: dayofweek = "Tuesday"
+elif day is 3: dayofweek = "Wednesday"
+elif day is 4: dayofweek = "Thursday"
+elif day is 5: dayofweek = "Friday"
+elif day is 6: dayofweek = "Saturday"
+elif day is 7: dayofweek = "Sunday"
+else: dayofweek = "not a valid entry"
 
 print("The day of the week you have entered is", dayofweek)
